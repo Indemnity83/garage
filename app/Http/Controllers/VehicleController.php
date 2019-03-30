bc1401616a354c5cc6e577b023a565f4ea908d1c
Kyle Klaus <kklaus@indemnity83.com>
2019-03-29 22:09:45 -0700

    feat: add vehicle api

---
+++ app/Http/Controllers/VehicleController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Vehicle;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class VehicleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json(Vehicle::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(Request $request)
+    {
+        $attributes = $this->validate($request, [
+            'name' => ['required'],
+            'vin' => [],
+            'license' => [],
+            'make' => [],
+            'model' => [],
+            'year' => ['integer'],
+            'notes' => [],
+        ]);
+
+        $vehicle = Vehicle::create($attributes);
+
+        return response()->json($vehicle, Response::HTTP_CREATED);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Vehicle  $vehicle
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Vehicle $vehicle)
+    {
+        return response()->json($vehicle);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \App\Vehicle $vehicle
+     * @return \Illuminate\Http\Response
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function update(Request $request, Vehicle $vehicle)
+    {
+        $attributes = $this->validate($request, [
+            'name' => ['required'],
+            'vin' => [],
+            'license' => [],
+            'make' => [],
+            'model' => [],
+            'year' => ['integer'],
+            'notes' => [],
+        ]);
+
+        $vehicle->update($attributes);
+
+        return response()->json($vehicle);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Vehicle $vehicle
+     * @return \Illuminate\Http\Response
+     * @throws \Exception
+     */
+    public function destroy(Vehicle $vehicle)
+    {
+        $vehicle->delete();
+
+        return response(null, Response::HTTP_NO_CONTENT);
+    }
+}
---
+++ database/migrations/2019_03_30_042907_create_vehicles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVehiclesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('vin')->nullable();
+            $table->string('license')->nullable();
+            $table->string('make')->nullable();
+            $table->string('model')->nullable();
+            $table->integer('year')->nullable();
+            $table->text('notes')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehicles');
+    }
+}
