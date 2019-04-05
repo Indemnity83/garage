62f317da7311ccb4fda7382835ea53db9f0c907d
Kyle Klaus <kklaus@indemnity83.com>
2019-04-05 22:19:39 +0000

    Apply fixes from StyleCI

--- routes/channels.php
+++ routes/channels.php
@@ -1,15 +1,12 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Broadcast Channels
-|--------------------------------------------------------------------------
-|
-| Here you may register all of the event broadcasting channels that your
-| application supports. The given channel authorization callbacks are
-| used to check if an authenticated user can listen to the channel.
-|
-*/
+ * Garage -- A code kata for the automobile owner
+ *
+ * Copyright (C) 2019 Kyle Klaus <kklaus@indemnity83.com>
+ *
+ * This software may be modified and distributed under the terms of the MIT license.  See the LICENSE file for details.
+ */
 
 Broadcast::channel('App.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
