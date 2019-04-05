373a874b36ae07748c1c11a61288ddd93d5ad35e
Kyle Klaus <kklaus@indemnity83.com>
2019-04-05 21:20:57 +0000

    Apply fixes from StyleCI

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -1,5 +1,13 @@
 <?php
 
+/*
+ * Garage -- A code kata for the automobile owner
+ *
+ * Copyright (C) <2019> <Kyle Klaus>
+ *
+ * This software may be modified and distributed under the terms of the MIT license.  See the LICENSE file for details.
+ */
+
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Route;
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,12 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+ * Garage -- A code kata for the automobile owner
+ *
+ * Copyright (C) <2019> <Kyle Klaus>
+ *
+ * This software may be modified and distributed under the terms of the MIT license.  See the LICENSE file for details.
+ */
 
 Route::get('/', function () {
     return view('welcome');
