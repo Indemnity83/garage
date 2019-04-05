60f621fbca8ca77a9f70b5a5b8affcd1211b3ed0
Kyle Klaus <kklaus@indemnity83.com>
2019-04-05 21:22:40 +0000

    Apply fixes from StyleCI

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -1,5 +1,13 @@
 <?php
 
+/*
+ * Garage -- A code kata for the automobile owner
+ *
+ * Copyright (C) 2019 Kyle Klaus <kklaus@indemnity83.com>
+ *
+ * This software may be modified and distributed under the terms of the MIT license.  See the LICENSE file for details.
+ */
+
 namespace App\Http;
 
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,15 +1,12 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Create The Application
-|--------------------------------------------------------------------------
-|
-| The first thing we will do is create a new Laravel application instance
-| which serves as the "glue" for all the components of Laravel, and is
-| the IoC container for the system binding all of the various parts.
-|
-*/
+ * Garage -- A code kata for the automobile owner
+ *
+ * Copyright (C) 2019 Kyle Klaus <kklaus@indemnity83.com>
+ *
+ * This software may be modified and distributed under the terms of the MIT license.  See the LICENSE file for details.
+ */
 
 $app = new Illuminate\Foundation\Application(
     $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
